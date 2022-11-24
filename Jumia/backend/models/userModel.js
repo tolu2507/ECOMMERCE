074de4f457f3c698b769893f6200fa88f307cd31
tolu2507 <tolusnehhil@gmail.com>
2022-11-24 11:50:18 +0100

    new commits

--- Jumia/backend/models/userModel.js
+++ Jumia/backend/models/userModel.js
@@ -4,7 +4,7 @@ const userSchema = new mongoose.Schema({
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    isAdmin: { type: Boolean, reuired: true, default: false }
+    isAdmin: { type: Boolean, required: true, default: false }
 });
 
 const userModel = mongoose.model("User", userSchema);
